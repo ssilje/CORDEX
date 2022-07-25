f22d04d7117a80463917b39e7f875ee292168de8
Silje Sørland <ssor@norceresearch.no>
2022-07-25 17:10:18 +0200

    Update seasonal_temp.py

--- seasonal_temp.py
+++ seasonal_temp.py
@@ -1,16 +1,19 @@
 import xarray as xr
-import os
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import cartopy.crs as ccrs
 
-
+# Read the data
 path = '/nird/projects/NS9001K/sso102/CORDEX/data/tas/EUR-11/rcp85'
 ncfile = xr.open_dataset(path + '/tas_mon_EUR-11_CLMcom_CCLM4-8-17_MPI-M-MPI-ESM-LR_r1i1p1_rcp85_1971-2099_SouthNorway.nc')
+
+# calculate area mean
+
 mean_data= ncfile.mean(dim='rlon').mean(dim='rlat').drop('lon_bnds').drop('lat_bnds').drop('rotated_latitude_longitude').drop('height')
 
-
+# Make a dataframe with month vs year
 
 time_year = pd.to_datetime(mean_data.time.values).strftime('%Y').unique()
 y = np.zeros(len(time_year))
@@ -32,48 +35,17 @@ d = {"Jan": pd.Series(y, index=time_year),
 
 df = pd.DataFrame(d)
 
+# Sorth the data by month and place them in the dataframe
 aa=list(mean_data.groupby('time.month'))   
 for m,(mm,mmm) in enumerate(aa): #loop through each month
-  print('m')
-  print(m)
-  print('mm')
-  print(mm)
-  print('mmm')
-  print(mmm)
   df.iloc[:,m]= mmm.tas.values  
 
 
 ## Plotting 
 
-varplot = ncfile.tas.sel(time='1971-01-01')
-#levels_plot = np.linspace(round(np.nanmin(dataopen_ymonmean.swvl1)),round(np.nanmax(dataopen_ymonmean.swvl1)),21)
-#levels_cbar = np.linspace(round(np.nanmin(dataopen_ymonmean.swvl1)),round(np.nanmax(dataopen_ymonmean.swvl1)),11)
-levels_plot = np.linspace(240,300,21)
-levels_cbar = np.linspace(240,300,11)
-plot_title  = 'test (1971) ' + varplot.long_name
-fname       = 'tas'
-label_text  = varplot.units
-
-im = varplot.plot(
-  x               = 'lon',
-  y               = 'lat',
-  col              = 'time',
-  col_wrap         = 3,
-  levels           = levels_plot,
-  subplot_kws      = dict(projection=ccrs.PlateCarree()),
-  transform        = ccrs.PlateCarree(),
-  cbar_kwargs      = {'label': label_text, 'ticks': levels_cbar}
-        )
-
-  
-for i,ax in enumerate(im.axes.flat):
-  ax.coastlines(resolution = '10m', 
-                color      = 'black',
-                linewidth  = 0.2)
-  ax.set_title(dataopen_ymonmean.time[i].dt.month.values)
-        
-plt.suptitle(plot_title)
-
-plt.savefig(fname+'.png',dpi='figure',bbox_inches='tight')
-plt.close()
-print('Figure stored at: '+fname+'.png')
+plt.clf()
+plt.contourf(np.linspace(1, 12,12), np.linspace(1971, 2099, 129), df.values, cmap='magma_r');
+plt.xticks(np.linspace(1, 12,12),df.columns, rotation='vertical')
+plt.ylabel('Year')  
+plt.colorbar();
+plt.savefig('tas_month_vs_year.png')
